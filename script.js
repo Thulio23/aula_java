e39027b0912d6d8e4f69540b3d5575dad185e6c3
THULIO23 <THULIOVICTOR23@GMAIL.COM>
2023-06-02 08:20:35 -0300

    aula02

--- script.js
+++ script.js
@@ -2,12 +2,52 @@ const listElement = document.querySelector("#app ul")
 const inputElement = document.querySelector("#app input")
 const buttonElement = document.querySelector("#app button")
 
-const tarefas = ["Estudar para prova" , "Dormir cedo"]
+const tarefas = JSON.parse(localStorage.getItem("list")) || []
 
-for (const iterator of tarefas){
-    const tarefaElement = document.createElement("li")
-    const tarefaText = document.createTextNode(iterator)
+//listar a lista
+function renderTarefas() {
+    listElement.innerHTML = ""
+    for (const iterator of tarefas) {
+        console.log(iterator)
+        const tarefaElement = document.createElement("li")
+        const tarefaText = document.createTextNode(iterator)
 
-    tarefaElement.appendChild(tarefaText)
-    listElement.appendChild(tarefaElement)
+        //deletar um elemento da lista
+        const linkElement = document.createElement("a")
+        linkElement.setAttribute("href", "#")
+
+        const pos = tarefas.indexOf(iterator)
+        linkElement.setAttribute("onclick", `deleteTarefa(${pos})`)
+
+        const linkText = document.createTextNode(" excluir")
+        linkElement.appendChild(linkText)
+
+        //appends
+        tarefaElement.appendChild(tarefaText)
+        tarefaElement.appendChild(linkElement)
+        listElement.appendChild(tarefaElement)
+    }
+}
+renderTarefas()
+
+//adicionar item à lista
+function addTarefas() {
+    const tarefaText = inputElement.value
+    console.log(tarefaText)
+    tarefas.push(tarefaText)
+    renderTarefas()
+    saveToStorage()
+}
+buttonElement.onclick = addTarefas
+
+//funcionar o excluir
+function deleteTarefa(pos){
+    tarefas.splice(pos, 1)
+    renderTarefas()
+    saveToStorage()
+}
+
+//salvar elementos
+function saveToStorage() {
+    localStorage.setItem("list", JSON.stringify(tarefas))
 }
